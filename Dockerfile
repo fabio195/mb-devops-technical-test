57a1328be90bfcdb3e7d9ffaa6741b169c9228c9
Fabio Muñoz <fabionahumunoz2@gmail.com>
2024-08-31 13:40:35 +0200

    Adding logging and error handling

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,6 @@ COPY ./requirements.txt /code/requirements.txt
 
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
-COPY .env /code/app/.env
 COPY ./main.py /code/app/main.py
 
 CMD ["fastapi", "run", "app/main.py", "--port", "80"]
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import logging
 import os
 
 import requests
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 load_dotenv()
 
@@ -11,19 +12,47 @@ SWAPI_URL = os.getenv("SWAPI_URL")
 
 app = FastAPI()
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 @app.get("/")
 def root():
+    logger.info("Root endpoint called")
     return {"message": "Welcome to the SWAPI API!"}
 
 
 @app.get("/people")
 def get_people():
-    response = requests.get(SWAPI_URL + "/people")
-    people_data = response.json()
+    try:
+        logger.info("Fetching people data from SWAPI")
 
-    # Sort people by "name" attribute
-    people_sorted_by_name = sorted(
-        people_data["results"], key=lambda person: person["name"]
-    )
-    return {"people_sorted_by_name": people_sorted_by_name}
+        if not SWAPI_URL:
+            logger.error("SWAPI_URL is not set")
+            raise HTTPException(
+                status_code=500, detail="Internal server error: SWAPI_URL is not set"
+            )
+
+        response = requests.get(SWAPI_URL + "/people")
+        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
+
+        people_data = response.json()
+
+        # Sort people by "name" attribute
+        people_sorted_by_name = sorted(
+            people_data["results"], key=lambda person: person["name"]
+        )
+
+        logger.info("Successfully fetched and sorted people data")
+        return {"people_sorted_by_name": people_sorted_by_name}
+
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error fetching data from SWAPI: {e}")
+        raise HTTPException(
+            status_code=502, detail="Bad Gateway: Error fetching data from SWAPI"
+        )
+
+    except Exception as e:
+        logger.error(f"Unexpected error: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
